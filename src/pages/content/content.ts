5529c0b0c04466806c5acf95f740198ec66e9cd0
andreu55 <anduwet2@gmail.com>
2018-02-09 22:25:26 +0100

    Listado de gastos borrables

--- src/pages/content/content.ts
+++ src/pages/content/content.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, ToastController, LoadingController } from 'ionic-angular';
 
 import { Api } from '../../providers/api/api';
 
@@ -12,17 +12,62 @@ export class ContentPage {
 
   api_token:string;
 
+  gastos: [
+    {
+      id:number,
+      total:string,
+      base:string,
+      desgr:string,
+      iva:string,
+      concepto:string,
+      fecha:string
+    }
+  ];
+
   constructor(
     public navCtrl: NavController,
     public toastCtrl: ToastController,
+    public loadingCtrl: LoadingController,
     public api: Api
   ) {
     this.api_token = localStorage.getItem("api_token");
   }
 
-  borraGasto(gasto_id) {
+  ionViewWillEnter() {
+    let loading = this.loadingCtrl.create({
+      content: 'Cargando gastos...'
+    });
+    loading.present();
 
-    console.log(gasto_id);
+    let headers = {
+      'headers': {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.api_token
+      }
+    };
+
+    let seq = this.api.post('gastos/get', {}, headers).share();
+
+    seq.subscribe((res: any) => {
+
+      if (res.status == 'success') {
+        this.gastos = res.gastos;
+        loading.dismiss();
+      }
+    }, err => {
+      // console.error('ERROR', err);
+      let toast = this.toastCtrl.create({
+        message: "Error",
+        duration: 2000,
+        position: 'bottom'
+      });
+      toast.present();
+    });
+
+    // return seq;
+  }
+
+  borraGasto(gasto_id) {
 
     let headers = {
       'headers': {
@@ -36,15 +81,8 @@ export class ContentPage {
     seq.subscribe((res: any) => {
 
       if (res.status == 'success') {
-        let toast = this.toastCtrl.create({
-          message: "¡Gasto borrado!",
-          duration: 2000,
-          position: 'bottom',
-          showCloseButton: true,
-          closeButtonText: 'yeah!'
-        });
-        toast.present();
-      } else {
+        // this.gastos
+        // $scope.items.splice(index, 1);
       }
     }, err => {
       // console.error('ERROR', err);
